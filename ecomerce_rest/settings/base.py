46520606f32d948e0cac3f75032e3278e6f780e9
morbid-face <jgallegovillada@gmail.com>
2021-07-29 17:42:34 -0500

    Setting .gitignore for Django SECRET_KEY

--- ecomerce_rest/settings/base.py
+++ ecomerce_rest/settings/base.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x6+^fe@mote&-ky-y-hx)znuqo1(vpc)$gv%+((deom9yym3xg'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
